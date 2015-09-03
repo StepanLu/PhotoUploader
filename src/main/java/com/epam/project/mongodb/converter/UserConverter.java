a23d33e8c81040fe1caa3cdffcf4ab90b151aa62
Stepan <stepan.lu@tut.by>
2015-09-03 22:00:58 +0300

    sdaffdgdsfg

--- src/main/java/com/epam/project/mongodb/converter/UserConverter.java
+++ src/main/java/com/epam/project/mongodb/converter/UserConverter.java
@@ -1,7 +1,33 @@
 package com.epam.project.mongodb.converter;
 
+import com.epam.project.domain.User;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+import org.bson.types.ObjectId;
+
 /**
  * Created by StepLuch on 03.09.15.
  */
 public class UserConverter {
+    public static DBObject toDBObject(User user){
+        BasicDBObjectBuilder builder = BasicDBObjectBuilder.start()
+                .append("firstName", user.getFirstName())
+                .append("lastName", user.getLastName())
+                .append("email", user.getEmail())
+                .append("password",user.getPassword())
+                .append("role",user.getRole())
+                .append("login", user.getLogin());
+        if (user.getId() != null)
+            builder = builder.append("_id", new ObjectId(user.getId()));
+        return builder.get();
+    }
+
+    public static User toUser(DBObject doc) {
+        User user = new User();
+        user.setFirstName((String) doc.get("firstName"));
+        user.setLastName((String) doc.get("lastName"));
+        ObjectId id = (ObjectId) doc.get("_id");
+        user.setId(id.toString());
+        return user;
+    }
 }
